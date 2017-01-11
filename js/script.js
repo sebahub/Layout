658bcc4cd518dc4a956f776b67648006ec591ebc
sebahub <annika@Jonas-iMac.local>
2017-01-11 17:31:50 +0100

    mobile_nav changed
    +Kontaktformular

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@ $(document).ready(function(){
     $(".nav_mobile").click(function(){
         $(".nav_mobile ul").toggleClass("visible");
     });
-});
 
-$(document).ready(function(){
-$('.burger').on("click", function (event) {
+
+    $('.nav_mobile').on("click", function (event) {
     $('span').toggleClass('rota');
     $('span').toggleClass('rota_reset');
 });
+
+
+
     });
